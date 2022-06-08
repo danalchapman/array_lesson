e0c8f4f2be129b3dda6643368e460fe220a78d6c
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-06-08 12:34:02 -0500

    Describe Index Positions

--- arrays.js
+++ arrays.js
@@ -6,7 +6,8 @@ console.log(charNames.pop());
 console.log(charNames);
 
 // Array of Numbers
-// Notes: The slice Array method will create a new Array of the chosen elements in that Index Position.
+// Notes: The slice Array method will create a new Array of the chosen elements
+// in that Index Position.
 
 var noteableAges = [13, 18, 21, 25, 30];
 console.log(noteableAges.slice(1));
@@ -18,3 +19,7 @@ console.log(noteableAges);
 var inClass = [true, false, false, true, false];
 console.log(inClass.shift());
 console.log(inClass);
+
+// Index Positions: The position of an element within an Array that is indicated
+// by a interger, starting with 0 as the first element and following through
+// with each element in the Array.
