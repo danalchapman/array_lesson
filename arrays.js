20a53db93a58cd8a21fbd012c532ddd60a6d7143
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-06-08 12:15:41 -0500

    Add Arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//  Array of Strings
+
+var charNames = ["ObiWan", "Satine", "Ahsoka", "Anakin", "Padme"];
+
+// Array of Numbers
+
+var noteableAges = [13, 18, 21, 25, 30];
+
+// Array of Booleans
+
+var inClass = [true, false, false, true, false];
